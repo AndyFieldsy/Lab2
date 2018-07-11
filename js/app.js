2fa31118d9f93eac90c5b0da28453e9f1693a4c1
Andrew Fields <andyfieldsy89@gmail.com>
2018-07-11 12:20:23 -0700

    I've made the changes to Lab2.

--- js/app.js
+++ js/app.js
@@ -21,48 +21,113 @@
 
 //Lab 2 Code Starts Here
 
-alert('Please answer all questions with either Y or N.')
+'use strict';
+
+alert('Please answer all questions with either Y or N.');
 var userQ1 = prompt('Do you like Pokemon?');
-console.log('here')
+console.log('here');
 if (userQ1.toUpperCase() === 'Y') {
-    alert("You said you liked Pokemon.")
+  alert('You said you liked Pokemon.');
 } else if (userQ1.toUpperCase() === 'N') {
-    alert("You do not like Pokemon. Boo.") 
-};
-console.log('Does the user like pokemon:',userQ1)
+  alert('You do not like Pokemon. Boo.');
+}
+console.log('Does the user like pokemon:',userQ1);
 
 
-    var userQ2 = prompt('Do you like baseball?')
-    if (userQ2.toUpperCase() === 'Y') {
-        alert('You like baseball.')
-    } else if (userQ2.toUpperCase() === 'N') {
-        alert('You do not like baseball.')
-    };
-    console.log('Does the user like baseball:',userQ2)
+var userQ2 = prompt('Do you like baseball?');
+if (userQ2.toUpperCase() === 'Y') {
+  alert('You like baseball.');
+}
+else if (userQ2.toUpperCase() === 'N') {
+  alert('You do not like baseball.');
+}
+console.log('Does the user like baseball:',userQ2);
 
 
-    var userQ3 = prompt('Do you like Subway sandwiches?')
-    if (userQ3.toUpperCase() === 'Y') {
-        alert('You do like Subway!')
-    } else if (userQ3.toUpperCase() === 'N') {
-        alert('You do not like Subway.')
-    };
-    console.log('Does the user like Subway:',userQ3)
+var userQ3 = prompt('Do you like Subway sandwiches?');
+if (userQ3.toUpperCase() === 'Y') {
+  alert('You do like Subway!');
+}
+else if (userQ3.toUpperCase() === 'N') {
+  alert('You do not like Subway.');
+}
+console.log('Does the user like Subway:',userQ3);
 
 
-    var userQ4 = prompt('Would a woodchuck chuck wood if he could?')
-    if (userQ4.toUpperCase() === 'Y') {
-        alert('Darn tootin he would')
-    } else if (userQ4.toUpperCase() === 'N') {
-        alert('Maybe he would not, you are right.')
-    };
-    console.log('Would a wood chuck chuck if able:',userQ4)
+var userQ4 = prompt('Would a woodchuck chuck wood if he could?');
+if (userQ4.toUpperCase() === 'Y') {
+  alert('Darn tootin he would');
+}
+else if (userQ4.toUpperCase() === 'N') {
+  alert('Maybe he would not, you are right.');
+}
+console.log('Would a wood chuck chuck if able:',userQ4);
 
 
-    var userQ5 = prompt('Is Luis\'s stories from Antman the best part of the movie?')
-    if (userQ5.toUpperCase() === 'Y') {
-        alert('You said yes.')
-    } else if (userQ5.toUpperCase() === 'N') {
-        alert('You accidently entered N instead of Y. Luis is always the best.')
-    };
-    console.log('Does the user like Luis\'s stories:',userQ5)
+var userQ5 = prompt('Is Luis\'s stories from Antman the best part of the movie?');
+if (userQ5.toUpperCase() === 'Y') {
+  alert('You said yes.');
+}
+else if (userQ5.toUpperCase() === 'N') {
+  alert('You accidently entered N instead of Y. Luis is always the best.');
+}
+console.log('Does the user like Luis\'s stories:',userQ5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var nums = [1,2,3,4,5,6,7,8,9];
+//     // for(start, stop, step) {}
+// for (var i = 0; i < nums.length; i++) {
+//     console.log('index: ', i)
+//     console.log('value: ', nums(i))
+// }
+
+
+// var multi = [[1,2,3], [4,5,6], [7,8,9]]
+
+// for ( var o = 0; o < multi.length, i++)
+//     for (var j = 0; j < multi[j].length; j++)
+//     console.log(multi[o][j])
+
+
+
+
+
+// var count = 0;
+// while (counter < 10) {
+//     console.log('count', count);
+//     count++;
+// }
+
+// console.log('done')
+
+// be careful of infinite loops
+
